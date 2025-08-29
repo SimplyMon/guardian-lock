af5076855b415246414589092e17227608fbb135
mon <mon.dev005@gmail.com>
2025-08-29 15:01:22 +0800

    firebase

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 
-// Firebase config v2
+// Firebase config 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
