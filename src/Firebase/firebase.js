9e97f322f69e5fcbf71bbf4587480fb1cb414da8
mon <mon.dev005@gmail.com>
2025-08-29 14:07:36 +0800

    fix env files gitignore

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,20 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 
-// Firebase config
+// Firebase config from .env
 const firebaseConfig = {
-  apiKey: "AIzaSyBoKLDspI4YeHJw0J6krS6tSN8WIvlT5sQ",
-  authDomain: "guardian-lock-fc1aa.firebaseapp.com",
-  databaseURL:
-    "https://guardian-lock-fc1aa-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "guardian-lock-fc1aa",
-  storageBucket: "guardian-lock-fc1aa.appspot.com",
-  messagingSenderId: "308099715960",
-  appId: "1:308099715960:web:05de8acac0d6cb08aa2e8d",
-  measurementId: "G-5R3XLJNYLS",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Main app
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
